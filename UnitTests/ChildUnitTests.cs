b84340dccb0e594964e8654c23ba884e850080f6
Tom Hatcher <39353602+TomH-NZ@users.noreply.github.com>
2025-06-11 11:50:58 +1200

    Add XUnit for unit testing, create first test.

---
+++ UnitTests/ChildUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using EliminatedChild;
+
+namespace UnitTests;
+
+public class ChildUnitTests
+{
+    [Fact]
+    public void GivenANumber_WhenCreatingAList_AListOfChildrenIsReturned()
+    {
+        //Arrange
+        var childData = 2;
+        var childList = new Child();
+        
+        //Act
+        var expected = new List<string>{"Child 1", "Child 2"};
+        var result = childList.ListToUse(childData);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
