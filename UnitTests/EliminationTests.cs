047d972d8ab1cc3300b1988491e66d915abc5d9d
Tom Hatcher <39353602+TomH-NZ@users.noreply.github.com>
2025-06-16 16:10:27 +1200

    Add initial code for Process file unit testing.

--- UnitTests/EliminationTests.cs
+++ UnitTests/EliminationTests.cs
@@ -1,6 +1,13 @@
-﻿namespace UnitTests;
+﻿using EliminatedChild;
+
+namespace UnitTests;
 
 public class EliminationTests
 {
+    IChildList childList = new ChildList();
+    static IApi childApi = new Api();
+
+    private DataObject childData = childApi.GetChildDataAsync();
+    
     
 }
